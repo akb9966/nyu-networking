9249d889ed1a4fc282e12ac268e22f72bddbe129
Arvind Bhat <akb9966@nyu.edu>
2021-02-19 00:37:27 -0500

    Web server assignment 1

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@
 #import socket module
 from socket import *
 import sys # In order to terminate the program
+import time
 
 def webServer(port=13331):
    serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -25,18 +26,26 @@ def webServer(port=13331):
          #Send one HTTP header line into socket
          #Fill in start
          header = "HTTP/1.1 200 OK\n"
+         time_now = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+         header += 'Date: {now}\n'.format(now=time_now)
+         header += 'Server: Simple-Python-Server\n'
+         header += 'Connection: close\n\n'
          connectionSocket.send(header.encode())
-
          #Fill in end
          #Send the content of the requested file to the client
          for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
+            print(outputdata[i].encode())
          connectionSocket.send("\r\n".encode())
          connectionSocket.close()
       except IOError:
          #Send response message for file not found (404)
          #Fill in start
-         header = "HTTP/1.1 404 Not Found\n"
+         header = 'HTTP/1.1 404 Not Found\n'
+         time_now = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+         header += 'Date: {now}\n'.format(now=time_now)
+         header += 'Server: Simple-Python-Server\n'
+         header += 'Connection: close\n\n'
          connectionSocket.send(header.encode())
          #Fill in end
          #Close client socket
