df59204fa2732d3c41e814f132e75adc215f24c6
Arvind Bhat <akb9966@nyu.edu>
2021-02-18 23:13:58 -0500

    Web server assignment 1

--- solution.py
+++ solution.py
@@ -1,33 +1,56 @@
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
+# Skeleton Python Code for the Web Server
+#import socket module
+from socket import *
+import sys # In order to terminate the program
 
-def welcome_assignment_answers(question):
-   # The student doesn't have to follow the skeleton for this assignment.
-   # Another way to implement it is using "case" statements similar to C.
-   if question == "Are encoding and encryption the same? - Yes/No":
-       answer = "No"
-   elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-       answer = "No"
-   elif question == "Is it possible to decode a message without a key? - Yes/No":
-       answer = "Yes"
-   elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-       answer = "No"
-   elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-       answer = "42b76fe51778764973077a5a94056724"
-   elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-       answer = "No"       
-   elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-       answer = 5
-   elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-       answer = 4
-   return (answer)
+def webServer(port=13331):
+   serverSocket = socket(AF_INET, SOCK_STREAM)
 
+   #Prepare a sever socket
+   #Fill in start
+   serverSocket.bind((gethostname(), port))
+   print(gethostname())
+   serverSocket.listen(5)
+   #Fill in end
 
-# Complete all the questions.
+   while True:
+      #Establish the connection
+      print('Ready to serve...')
+      connectionSocket, addr = serverSocket.accept()
+      try:
+         message = connectionSocket.recv(1024).decode()
+         print(message)
+         filename = message.split()[1]
+         print(filename)
+         print("filename printed...........")
+         f = open(filename[1:])
+         outputdata = f.read()
+         print(outputdata)
+         print("output printed...........")
 
+         #Send one HTTP header line into socket
+         #Fill in start
+         header = "HTTP/1.1 200 OK\n"
+         connectionSocket.send(header.encode())
+
+         #Fill in end
+         #Send the content of the requested file to the client
+         for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+         connectionSocket.send("\r\n".encode())
+         connectionSocket.close()
+      except IOError:
+         #Send response message for file not found (404)
+         #Fill in start
+         header = "HTTP/1.1 404 Not Found\n"
+         connectionSocket.send(header.encode())
+         #Fill in end
+         #Close client socket
+         #Fill in start
+         connectionSocket.close()
+         #Fill in end
+      serverSocket.close()
+      sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-   # use this space to debug and verify that the program works
-   debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-   print(welcome_assignment_answers(debug_question))
+   webServer(13331)
