4ebaa4b29807c315b1bce5e831085c855aba0219
Arvind Bhat <akb9966@nyu.edu>
2021-02-19 00:40:12 -0500

    Web server assignment 1

--- solution.py
+++ solution.py
@@ -35,7 +35,6 @@ def webServer(port=13331):
          #Send the content of the requested file to the client
          for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
-            print(outputdata[i].encode())
          connectionSocket.send("\r\n".encode())
          connectionSocket.close()
       except IOError:
